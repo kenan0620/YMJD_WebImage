c35ff917d3688db33e7c838a45caf5ffbf13233d
houkn <houkn@wdcloud.cc>
2019-02-20 11:14:52 +0800

    1902201114、文档同步
    1、说明文档同步完成

--- YMJD_WebImage/YMJD_WebImage/UIImageView+WDYWebImage.h
+++ YMJD_WebImage/YMJD_WebImage/UIImageView+WDYWebImage.h
@@ -5,7 +5,7 @@
 //  Created by mac on 2019/2/18.
 //  Copyright © 2019 伟东云教育. All rights reserved.
 //
-
+//MAC开发中需要添加Transition，
 #import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
@@ -14,23 +14,41 @@ typedef void(^WDYWebImageDownloaderProgressBlock)(NSInteger receivedSize, NSInte
 
 @interface UIImageView (WDYWebImage)
 
+/**
+ 设置图片
+ 
+ @param url 图片地址
+
+ */
 - (void)wdy_setImageWithURL:(nullable NSURL *)url;
 
+/**
+ 设置图片、占位图
+ 
+ @param url 图片地址
+ @param placeholderImage 占位图
+ */
 - (void)wdy_setImageWithURL:(nullable NSURL *)url
            PlaceholderImage:(nullable UIImage *)placeholderImage;
 
+/**
+ 设置图片、占位图、进度
+ 
+ @param url 图片地址
+ @param placeholderImage 占位图
+ @param progress 进度
+ */
 - (void)wdy_setImageWithURL:(nullable NSURL *)url
            PlaceholderImage:(nullable UIImage *)placeholderImage
                    Progress:(nullable WDYWebImageDownloaderProgressBlock)progress;
 
 /**
- 设置图片
+ 设置图片、占位图、进度、回调
  
  @param url 图片地址
  @param placeholderImage 占位图
  @param progress 进度
  @param complete 完成回调
- 
  */
 - (void)wdy_setImageWithURL:(nullable NSURL *)url
            PlaceholderImage:(nullable UIImage *)placeholderImage
